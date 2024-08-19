05eb3d240c21eb6741478e946345c798a1192931
kelvinbobsin <kelvin.bobsin.1999@gmail.com>
2024-08-19 14:27:06 -0300

    Test

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     baseUrl:'http://localhost:5400'
   },
